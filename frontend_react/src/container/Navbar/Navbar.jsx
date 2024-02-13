5b95174aff7ea121803e3e723efa3f7dac146124
Dat <binhut123@gmail.com>
2024-02-12 21:21:08 -0800

    add style to Navbar

--- frontend_react/src/container/Navbar/Navbar.jsx
+++ frontend_react/src/container/Navbar/Navbar.jsx
@@ -10,7 +10,25 @@ const Navbar = () => {
 
 const [toggle, setToggle] = useState(false);
 return (
+ 
     <nav className='app__navbar'>
+        <div className="app__navbar-menu">
+            {!toggle && (<HiMenuAlt4 onClick={() => setToggle(true)} />)}
+            {toggle && (<HiX onClick={() => setToggle(false)} />)}
+            {toggle && (
+            <motion.div
+                whileInView={{ x: [0, 0] }}
+                transition={{ duration: .75, ease: 'easeInOut' }}
+            >            
+                <ul className='app__navbar-links'>
+                    <li className='app__flex p-text'><a href="#home">Home</a></li>
+                    <li className='app__flex p-text'><a href="#sve">SVE Products</a></li>
+                    <li className='app__flex p-text'><a href="#cfv">CFV Products</a></li>
+                    <li className='app__flex p-text'><a href="#weiss">Weiss Schwarz Products</a></li>
+                </ul>
+            </motion.div>
+            )}
+        </div>
         <div className='app__navbar-logo'>
             <img src={images.logo} alt="Store logo" />
         </div>
@@ -21,25 +39,7 @@ return (
             <li className='app__flex p-text'><div /><a href="#weiss">Weiss Products</a></li>
         </ul>
         <AiOutlineShoppingCart size={60} />
-        <div className="app__navbar-menu">
-            <HiMenuAlt4 onClick={() => setToggle(true)} />
-            
-            {toggle && (
-            <motion.div
-                whileInView={{ x: [300, 0] }}
-                transition={{ duration: 0.85, ease: 'easeOut' }}
-            >
-                <HiX onClick={() => setToggle(false)} />
-                <img class='center' src={images.logo} alt="Store logo" width={180} height={180}/>
-                <ul className='app__navbar-links'>
-                    <li className='app__flex p-text'><a href="#home">Home</a></li>
-                    <li className='app__flex p-text'><a href="#sve">SVE Products</a></li>
-                    <li className='app__flex p-text'><a href="#cfv">CFV Products</a></li>
-                    <li className='app__flex p-text'><a href="#weiss">Weiss Schwarz Products</a></li>
-                </ul>
-            </motion.div>
-            )}
-        </div>
+    
     </nav>
     
   )
